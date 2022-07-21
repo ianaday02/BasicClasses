aa8b53ad98b5fc14f8c0887e73ee6f7bd9ca8e23
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-21 14:44:10 -0500

    initial commit

---
+++ BasicClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Year = 2018;
+            myCar.Make = "Mazda";
+            myCar.Model = "CX-5";
+
+            Console.WriteLine($"I love going on road trips in my {myCar.Year} {myCar.Make} {myCar.Model}!");
+        }
+    }
+}
